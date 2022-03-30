c92a5f451531955e59030e89dbe21b8666dbc07a
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-30 16:55:50 +0530

    1 file modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import NotesList from './components/NotesList';
+import { nanoid } from 'nanoid';
+import Search from './Components/Search';
+import Header from './Components/Header';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <Search />
     </div>
   );
 }
